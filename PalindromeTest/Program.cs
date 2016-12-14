d669114d05ccc7008fcfcb535539ff3ad5eef686
Salomon Zhang <salomon1988nz@gmail.com>
2016-12-15 01:28:09 +1300

    First commit

---
+++ PalindromeTest/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PalindromeTest
+{
+    public class Program
+    {
+        /// <summary>
+        /// Accept a string from windows console and display a message
+        /// </summary>
+        public static void Main(string[] args)
+        {
+            Program test = new Program();
+
+            string s = "";
+            Console.WriteLine("Enter string");
+            s = Console.ReadLine();
+
+            test.IsAnagramOrPalindrome(s);
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Analyse the string is whether a palindrome, an anagram of a palindrome or neither
+        /// </summary>
+        /// <param name="s">Input string</param>
+        /// <returns>True if the input string is a palindrome or an anagram of a palindrome, else false</returns>
+        public bool IsAnagramOrPalindrome(string s)
+        {
+            char[] array = s.ToCharArray();
+            Array.Reverse(array);
+            string backwards = new string(array);
+
+            var charCount = s.GroupBy(c => c, (c, i) => new
+            {
+                character = c,
+                count = i.Count()
+            });
+
+            if (s == backwards) // If the string is same as the reversed string
+            {
+                Console.WriteLine("Entered string {0} is a palindrome", s);
+                return true;
+            }
+            else if (charCount.Count(c => c.count % 2 == 1) <= 1) //If the single character number of the string is 0 or 1
+            {
+                Console.WriteLine("Entered string {0} is an anagram of a palindrome", s);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Entered string {0} is neither a palindrome nor an anagram of a palindrome", s);
+                return false;
+            }
+
+        }
+    }
+}
---
+++ UnitTest/UnitTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PalindromeTest;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        Program p = new Program();
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            bool expected = true;
+            bool actual = p.IsAnagramOrPalindrome("anna");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            bool expected = true;
+            bool actual = p.IsAnagramOrPalindrome("aaaaaaaa");
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            bool expected = true;
+            bool actual = p.IsAnagramOrPalindrome("aaaaaaaab");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            bool expected = false;
+            bool actual = p.IsAnagramOrPalindrome("caaaaaaaab");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            bool expected = true;
+            bool actual = p.IsAnagramOrPalindrome("donotbobtonod");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            bool expected = false;
+            bool actual = p.IsAnagramOrPalindrome("owefhijpfwai");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            bool expected = true;
+            bool actual = p.IsAnagramOrPalindrome("igdedgide");
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
